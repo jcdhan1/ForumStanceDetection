3422c031b05d738ccd52c16ca9162a5472702a46
ACA15JCH <jcdhan1@sheffield.ac.uk>
2018-03-03 22:07:06 +0000

    Refactored the skipgram method

--- readwrite/writer.py
+++ readwrite/writer.py
@@ -14,31 +14,21 @@ class Writer:
     def __init__(self, train_data, test_data, out_path):
         """
         A class for vectorising data from forum posts.
-        
         """
         self.train_data = train_data
         self.test_data = test_data
         self.out_path = out_path
     
-    def filterStopwords(self,tokenised_body):
-        """
-        Remove stopwords from a tokenised body
-        :param tokenised_body: tokenised body
-        :return: tokens without stopwords
-        """
-        stops = nltk.corpus.stopwords.words("english")
-        stops.extend(["\"", "#", "$", "%", "&", "\\", "'", "(", ")", "*", ",", "-", ".", "/", ":",
-                      ";", "<", ">", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "=", "+", "!",  "?"])
-        stops.extend(["rt", "#semst", "...", "thats", "im", "'s", "via"])
-        stops = set(stops)
-        return [w for w in tokenised_body if (not w in stops and not w.startswith("http"))]
-
-    def tokenise_body(self, body):
-        return self.filterStopwords(tokenize(body.lower()))
-    
     def skipgram(self, context, num_workers, num_features):
+        """
+        Generate a skip-gram file.
+        
+        :param context     : context window size
+        :param num_workers : the number of workers
+        :param num_features: the number of features
+        """
         #Tokenise
-        tokenized_posts = list(map(lambda p: self.tokenise_body(p.body),self.train_data.post_list))
+        tokenized_posts = list(map(lambda p: filterStopwords(tokenize(p.body.lower())),self.train_data.post_list))
         #Vectorise
         model_sg = Word2Vec(sentences=tokenized_posts,
                         size=num_features, 
@@ -66,7 +56,6 @@ class Writer_X1(Writer):
     def save_model(self, model, extension):
         """
         Models for experiment setup 1 are saved in the directory experiment_1 and a subdirectory named after the topic. The prefix of the testing data is the filename.
-        
         """
         model.save(self.out_path + 'experiment_1/' + self.train_data.topic + '/' + self.prefix + extension)
         
@@ -86,15 +75,34 @@ class Writer_X2(Writer):
         """
         model.save(self.out_path + 'experiment_2/' + self.train_data.topic + extension)
 
+def filterStopwords(tokenised_body):
+    """
+    Remove stopwords from a tokenised body
+    
+    :param tokenised_body: tokenised body
+    :return: tokens without stopwords
+    """
+    stops = nltk.corpus.stopwords.words("english")
+    stops.extend(["\"", "#", "$", "%", "&", "\\", "'", "(", ")", "*", ",", "-", ".", "/", ":",
+                  ";", "<", ">", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "=", "+", "!",  "?"])
+    stops.extend(["rt", "#semst", "...", "thats", "im", "'s", "via"])
+    stops = set(stops)
+    return [w for w in tokenised_body if (not w in stops and not w.startswith("http"))]
+
 if __name__ == '__main__':
     #Instantiate a Reader
-    reader1 = reader.Reader('../data/CreateDebate/', '../data/fourforums/')
-    a_dbt = reader.load_cd(topic_dir="marijuana", prefix='A', exclude=False)
-    not_a_dbt = reader.load_cd(topic_dir="marijuana", prefix='A', exclude=True)
-    all_marijuana = reader.load_cd(topic_dir="marijuana", prefix="ALL")
+    rdr = reader.Reader('../data/CreateDebate/', '../data/fourforums/')
     
-    ex1gen = Writer_X1(not_a_dbt, a_dbt, "../out", "marijuana", "A")
+    seen_target = "marijuana"
+    unseen_target = "marijuana legalization"
+    
+    a_dbt = rdr.load_cd(seen_target, 'A')
+    not_a_dbt = rdr.load_cd(seen_target, 'A', True)
+    all_marijuana = rdr.load_cd(seen_target, "ALL")
+    rndm_dbt = rdr.load_4f(unseen_target)
+    
+    ex1gen = Writer_X1(not_a_dbt, a_dbt, "../out/", "marijuana", "A")
     ex1gen.skipgram(15,4,151)
-    ex2gen = Writer_X2(all_marijuana, preprocess.Debate("marijuana legalization", [reader.load_4f()]), "../out/")
-    ex2gen.skipgram(15,4,151)
     
+    ex2gen = Writer_X2(all_marijuana, preprocess.Debate(unseen_target, [rndm_dbt]), "../out/")
+    ex2gen.skipgram(15,4,151)
