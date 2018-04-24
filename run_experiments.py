fc65810a34bedac36871252f79652a38e72ef986
ACA15JCH <jcdhan1@sheffield.ac.uk>
2018-04-24 02:19:16 +0100

    Output latex for experiment1

--- run_experiments.py
+++ run_experiments.py
@@ -8,8 +8,9 @@ import numpy as np
 import reader, writer, preprocess, copy, tabulate, argparse
 from twokenize_wrapper.twokenize import tokenize
 from sklearn import svm
+from gensim.models import Word2Vec
 from sklearn.preprocessing import scale
-from classifiers.hybrid import MultiOneClassifier
+import matplotlib.pyplot as plt
 
 STANCES = {'AGAINST': -1, 'NONE': 0, 'FAVOR': 1}
 
@@ -37,7 +38,7 @@ class Model_Wrapper:
         return list(map(lambda p: STANCES[p.label], debate.post_list))
     
 class Experiment:
-    def __init__(self, classifier, img_path, dir_cd):
+    def __init__(self, classifier, img_path, dir_cd, train_data={}, test_data={}, models={}):
         """
         Constructor for an Experiment.
         
@@ -48,31 +49,56 @@ class Experiment:
         self.classifier = classifier
         self.img_path = img_path
         self.dir_cd = dir_cd
+        self.test_data = test_data
+        self.train_data = train_data
+        self.models = models
+        
+    def bar_plot(self, dict_1, dict_2, name_1, name_2, classifier, m_name=''):
+        
+        N = len(dict_1)
+        val_1 = tuple(dict_1.values())
+        
+        ind = np.arange(N)
+        width = 0.35
+        
+        fig, ax = plt.subplots()
+        rects1 = ax.bar(ind, val_1, width, color='r')
+        
+        val_2 = tuple(dict_2.values())
+        rects2 = ax.bar(ind + width, val_2, width, color='b')
+        
+        ax.set_xticks(ind + width / 2)
+        ax.set_xticklabels(tuple(dict_1.keys()))
+        ax.set_ylim([0,1])
+        ax.legend((rects1[0], rects2[0]), (name_1, name_2))
+        
+        plt.savefig(self.img_path + self.img_directory() + '/' + x1_filename(classifier,name_1,name_2, m_name), format='svg')
+        plt.show()
+
 
 class Experiment1(Experiment):
-    def __init__(self, classifier, img_path, dir_cd, topic):
+    def __init__(self, classifier, img_path, dir_cd, topic, train_data={}, test_data={}, models={}):
         """
         A subclass of Experiment specifically for experiment setup 1.
         
         :param topic: The topic of all posts in the data-set.
         """
-        super(Experiment1, self).__init__(classifier, img_path, dir_cd)
+        super(Experiment1, self).__init__(classifier, img_path, dir_cd, train_data, test_data, models)
         self.topic = topic
+        
+    def img_directory(self):
+        return self.topic
     
     def evaluate(self, rdr):
         mets = {}
         accs = {}
         max_props = {}
         for prefix in reader.subsetAZ(self.dir_cd + self.topic):
-            print("Generating model")
-            train_data = rdr.load_cd(topic, prefix, True)
-            test_data = rdr.load_cd(topic, prefix)
-            wvm_gen = writer.Writer_X1(train_data, test_data, self.topic, prefix)
-            wvmodel = Model_Wrapper(wvm_gen.skipgram(15,4,151))
-            train_arrays = wvmodel.vectorise_debate(train_data)
-            test_arrays  = wvmodel.vectorise_debate(test_data)
-            train_labels = wvmodel.stance_to_int(train_data)
-            test_labels = wvmodel.stance_to_int(test_data)
+            wvmodel = self.models[prefix]
+            train_arrays = wvmodel.vectorise_debate(self.train_data[prefix])
+            test_arrays  = wvmodel.vectorise_debate(self.test_data[prefix])
+            train_labels = wvmodel.stance_to_int(self.train_data[prefix])
+            test_labels = wvmodel.stance_to_int(self.test_data[prefix])
             print("Training on", topic, "debates except for", prefix)
             self.classifier.fit(train_arrays,train_labels)
             print("Testing on", topic, "debate", prefix)
@@ -105,17 +131,20 @@ class Experiment1(Experiment):
         return mets, accs, max_props
     
 class Experiment1_5(Experiment):
-    def __init__(self, classifier, img_path, dir_cd, seen_target, unseen_target):
+    def __init__(self, classifier, img_path, dir_cd, seen_target, unseen_target, train_data={}, test_data={}, model={}):
         """
         A subclass of Experiment specifically for experiment setup 1.5
         
         :param seen_target  : the topic of the training data.
         :param unseen_target: the topic of the testing data.
         """
-        super(Experiment1_5, self).__init__(classifier, img_path, dir_cd)
+        super(Experiment1_5, self).__init__(classifier, img_path, dir_cd, train_data, test_data, models)
         self.seen_target = seen_target
         self.unseen_target = unseen_target
         
+    def img_directory(self):
+        return self.seen_target + self.unseen_target
+    
     def evaluate(self, rdr):
         train_data = rdr.load_cd(self.seen_target, 'ALL')
         test_data = rdr.load_cd(self.unseen_target, 'ALL')
@@ -149,9 +178,9 @@ class Experiment1_5(Experiment):
             proportion = train_labels.count(n)/len(train_labels)
             proportions+=[proportion]
             metrics[stance] = [precision, recall, f_measure, proportion]
-        
+        del metrics['NONE']
         return metrics, accuracy, max(proportions)
-
+    
 class Experiment2(Experiment):
     def __init__(self, classifier, img_path, dir_cd, dir_4f, seen_target, unseen_target, occ):
         """
@@ -202,7 +231,15 @@ class Experiment2(Experiment):
 def correctness(actual, predicted, class_n):
     return sum([(x[0]==x[1] and x[0]==class_n) for x in zip(actual, predicted)])
 
+def extract_metrics(mets, stance, m_int):
+    #m_int 0 for precision, 1 for recall, 2 for f-measure
+    return dict(map(lambda m: (m[0], m[1][stance][m_int]), mets.items()))
+
+def x1_filename(classifier, name_1, name_2, m_name=''):
+    return classifier + name_1 + name_2 + m_name + '.svg'
+    
 if __name__ == '__main__':
+    plt.rcParams['svg.fonttype'] = 'none'
     parser = argparse.ArgumentParser(description='Run experiments setup 1 or 1.5.')
     parser.add_argument('--classifier', '-c', nargs='?', default ='', help='The classifier to use.')
     parser.add_argument('--image', '-i', nargs='?', default ='', help='Where to output graphs.')
@@ -217,7 +254,7 @@ if __name__ == '__main__':
     classifier_names = ['liblinear', 'linear', 'Polynomial','Sigmoid','Radial Basis Function']
     classifier_dict = dict(zip(classifier_names, classifiers))
     valid_classifier = args['classifier'].lower() in classifier_names
-    classifier_choice = args['classifier'].lower() if valid_classifier else reader.select_opt(classifier_names, "Select a classifier:")
+    classifier_choice = args['classifier'].lower() if valid_classifier else reader.select_opt(classifier_names, "Select a classifier: ")
 
     dir_cd = input("Where are the posts from CreateDebate stored?") if not args['createdebate'] else args['createdebate'] #./data/CreateDebate/
     rdr=reader.Reader(dir_cd)
@@ -227,17 +264,74 @@ if __name__ == '__main__':
     img_path = input("Where should graphs be exported to?") if not args['image'] else args['image']
     topic = rdr.select_topic() if args['topic'] not in rdr.dir_lst else args['topic']
     
+    if(input('reload data for experiment 1?').lower() == 'y'):
+        train_data1={}
+        test_data1={}
+        for prefix in reader.subsetAZ(dir_cd + topic):
+            print('Loading', prefix)
+            train_data1[prefix] = rdr.load_cd(topic, prefix, True)
+            test_data1[prefix] = rdr.load_cd(topic, prefix)
+    
+    wvm_gen = writer.Writer_X1(train_data1[prefix], test_data1[prefix], topic, prefix)
+    if(input('regenerate models for experiment 1?') == 'y'):
+        models1 = {}
+        print('Generating Models')
+        for prefix in reader.subsetAZ(dir_cd + topic):
+            print(prefix)
+            models1[prefix] =  Model_Wrapper(wvm_gen.skipgram(15,4,151))
+            models1[prefix].model.save('./out/experiment_1/' + topic + '/' + prefix + '.wv')
+    else:
+        print('Loading Models')
+        for prefix in reader.subsetAZ(dir_cd + topic):
+            print(prefix)
+            models1[prefix] =  Model_Wrapper(Word2Vec.load('./out/experiment_1/' + topic + '/' + prefix + '.wv'))
+    
     if setup_choice == 'setup 1':
         #Experiment 1 args: -i ./img/experiment_1/ -s 1 -d ./data/CreateDebate/
         classifier1 = copy.deepcopy(classifier_dict[classifier_choice]) 
-        experiment1 = Experiment1(classifier1,img_path,dir_cd,topic)
+        experiment1 = Experiment1(classifier1,img_path,dir_cd, topic, train_data1, train_data1, models1)
         mets, accs, max_props = experiment1.evaluate(rdr)
         print("Accuracy:", np.mean(list(accs.values())))
-        print("# of times accuracy was greater than proportion of most frequent stance in training data:", sum([accs[prefix] > max_props[prefix] for prefix in reader.subsetAZ(dir_cd + topic)]))
+        print("\\newline")
+        print("Number of times accuracy was greater than proportion of most frequent stance in training data:", sum([accs[prefix] > max_props[prefix] for prefix in reader.subsetAZ(dir_cd + topic)]))
         for prefix, metrics in mets.items():
+            del metrics['NONE']
+            print("\\newline")
             print(prefix)
-            print(accs[prefix])
-            print(tabulate.tabulate(metrics, headers="keys"))
+            print('\\newline')
+            print("Accuracy:", accs[prefix])
+            print('\\newline')
+            print(tabulate.tabulate(metrics, headers="keys", tablefmt="latex"))
+        print("\\begin{minipage}{\linewidth}")
+        print('    Accuracy')
+        print('    \\newline')
+        print("    \includesvg{img/experiment_1/" + topic + '/' + x1_filename(classifier_choice, 'accuracy','most-frequent-class') + "}")
+        print("\end{minipage}")
+        print("\\begin{minipage}{\linewidth}")
+        print('    Precision')
+        print('    \\newline')
+        print("    \includesvg{img/experiment_1/" + topic + '/' + x1_filename(classifier_choice, 'AGAINST','FAVOR', 'precision') + "}")
+        against_p = extract_metrics(mets, 'AGAINST', 0)
+        favor_p = extract_metrics(mets, 'FAVOR', 0)
+        print("\end{minipage}")
+        print("\\begin{minipage}{\linewidth}")
+        print('    Recall')
+        print('    \\newline')
+        print("    \includesvg{img/experiment_1/" + topic + '/' + x1_filename(classifier_choice, 'AGAINST','FAVOR', 'recall') + "}")
+        against_r = extract_metrics(mets, 'AGAINST', 1)
+        favor_r = extract_metrics(mets, 'FAVOR', 1)
+        print("\end{minipage}")
+        print("\\begin{minipage}{\linewidth}")
+        print('    F-Measure')
+        print('    \\newline')
+        print("    \includesvg{img/experiment_1/" + topic + '/' + x1_filename(classifier_choice, 'AGAINST','FAVOR', 'f-measure') + "}")
+        against_f = extract_metrics(mets, 'AGAINST', 2)
+        favor_f = extract_metrics(mets, 'FAVOR', 2)
+        print("\end{minipage}")
+        experiment1.bar_plot(accs, max_props, 'accuracy','most-frequent-class', classifier_choice)
+        experiment1.bar_plot(against_p, favor_p, 'AGAINST','FAVOR', classifier_choice, 'precision')
+        experiment1.bar_plot(against_r, favor_r, 'AGAINST','FAVOR', classifier_choice, 'recall')
+        experiment1.bar_plot(against_f, favor_f, 'AGAINST','FAVOR', classifier_choice, 'f-measure')
         
     else:
         #==============================================================================
@@ -260,4 +354,4 @@ if __name__ == '__main__':
         met, acc, max_prop = experiment1_5.evaluate(rdr)
         print("Accuracy:", acc)
         print("Proportion of Most Frequent Class in Training Data:", max_prop)
-        print(tabulate.tabulate(met, headers="keys"))
+        print(tabulate.tabulate(met, headers="keys", tablefmt="latex"))
